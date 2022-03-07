724ae200463d964c12c7dd1a4549a94549358984
KrutikVachhani <94003301+KrutikVachhani@users.noreply.github.com>
2022-03-07 19:08:29 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo{
+    public static void main(String[] any) {
+        System.out.print("Hello From Java\n");
+        System.out.print("Thank you for using Java");
+        System.out.print("enter any number");
+        int a=Sc.nextint();
+        System.out.print("a");
+    }
+}
